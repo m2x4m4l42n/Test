f2f04f16cd99dce2985e770f67164909afdc4ffd
m2x4m4l42n <max.vonall@gmail.com>
2016-06-20 15:51:55 +0200

    added modulo function to Test.java

--- Test.java
+++ Test.java
@@ -7,4 +7,11 @@ public class Test{
     if(b == 0) return a;
     else return euclid(b, a % b);
   }
+
+  public static int modulo(int a, int mod){
+    if(mod<=0) return -1;
+    if(a<0) return modulo(a+mod,mod);
+    if(a<mod) return a;
+    else return modulo(a-mod,mod);
+  }
 }
